58fab302018a9e6a3f4a518bcd55340d2296cde5
georgegoldberg1 <45336261+georgegoldberg1@users.noreply.github.com>
2022-06-12 17:03:15 +0100

    added clearer comments on each step in GO.sh file

--- go.sh
+++ go.sh
@@ -1,27 +1,29 @@
 #script to build and run jupyter in docker
 
+#use a unique port in each project, so containers don't conflict on the host
 port_to_run=8889
 
+#if one is already running, stop it:
 docker stop base_python
 
+#build the custom docker image
 docker build \
 	--tag py310:latest \
 	--label py310 \
 	--build-arg HOSTUSER="$(whoami)" \
 	.
 
+#remove old builds if they exist
 docker image prune --force --filter='label=py310'
 
-#open http://localhost:8889
-
-#docker run --rm -i \
+#start the container + jupyter
 docker run --rm -d \
 	--publish $port_to_run:8888 \
         --name base_python \
         -v "$(pwd)":"/home/$(whoami)/hostmachine" \
         py310
 
-# Initial browser window in order to generate token + cookie
+# Initial browser window in order to generate token + cookie. Increase 2nd sleep if needed.
 sleep 1
 open http://localhost:$port_to_run
 sleep 1
